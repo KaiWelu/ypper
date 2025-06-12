f78f91236523b669b2f3ed1b169402bfe7bca797
KaiWelu <kaiweluda@gmail.com>
2025-06-12 12:48:10 +0200

    adds logger

--- src/main/java/com/dci/ypper/YpperApplication.java
+++ src/main/java/com/dci/ypper/YpperApplication.java
@@ -1,13 +1,23 @@
 package com.dci.ypper;
 
+import com.book.spring_book.SpringBookApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YpperApplication {
+public class YpperApplication implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(SpringBookApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(YpperApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("===== YPPR IS UP AND RUNNING! =====");
 	}
 
 }
