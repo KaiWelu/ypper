c1897a0b50c171bc5360212ac08fd5cf67386a30
KaiWelu <kaiweluda@gmail.com>
2025-06-12 12:45:20 +0200

    adds comment model

--- src/main/java/com/dci/ypper/models/Comment.java
+++ src/main/java/com/dci/ypper/models/Comment.java
@@ -1,4 +1,29 @@
 package com.dci.ypper.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+
 }
