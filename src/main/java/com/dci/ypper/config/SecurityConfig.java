47830f1a585d3ca024ec16f0b9c2eca0c329378c
KaiWelu <kaiweluda@gmail.com>
2025-06-16 14:10:18 +0200

    adds security config

---
+++ src/main/java/com/dci/ypper/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.dci.ypper.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // this tells spring to use a custom security flow
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(customizer -> customizer.disable()); // disables crsf
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults()); // adds form login
+        httpSecurity.httpBasic(Customizer.withDefaults()); // adds basic http login
+        httpSecurity.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return httpSecurity.build();
+    }
+}
