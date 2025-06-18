f92ada1a47cd2c78cd33ca93b2e9881119ca4386
KaiWelu <kaiweluda@gmail.com>
2025-06-18 13:20:44 +0200

    refactors user controller

--- src/main/java/com/dci/ypper/config/SecurityConfig.java
+++ src/main/java/com/dci/ypper/config/SecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity // this tells spring to use a custom security flow
@@ -35,7 +40,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable()); // disables crsf
         httpSecurity.httpBasic(Customizer.withDefaults()); // adds basic http login
+
         httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/register", "/login")
                 .permitAll()
                 .anyRequest()
@@ -60,5 +67,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
--- src/main/java/com/dci/ypper/controller/UserController.java
+++ src/main/java/com/dci/ypper/controller/UserController.java
@@ -3,9 +3,13 @@ package com.dci.ypper.controller;
 import com.dci.ypper.model.User;
 import com.dci.ypper.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -18,8 +22,12 @@ public class UserController {
         return userService.register(user);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000/")
     @PostMapping("/login")
-    public String login(@RequestBody User user) {
-        return userService.verify(user);
+    public ResponseEntity<Map<String, String>> login(@RequestBody User user) {
+        System.out.println(user);
+        String token = userService.verify(user);
+        Map<String, String> response = Map.of("token", token);
+        return ResponseEntity.ok(response);
     }
 }
